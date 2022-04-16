d898753a5b2fab0bc5ed45f868a1bb102b05b028
KASTURIHARINI <101312562+KASTURIHARINI@users.noreply.github.com>
2022-04-16 20:42:59 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    float var1, var2, res;
+    scanf("%f%f",&var1,&var2);
+    res=var1+var2;
+    printf("%f",res);
+
+    return 0;
+}
